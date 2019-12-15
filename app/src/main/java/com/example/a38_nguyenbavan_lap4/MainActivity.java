3a4d93bc36956d73ac47436a74fda700a42071ea
vstarhp16 <58426305+vstarhp16@users.noreply.github.com>
2019-12-15 10:29:42 +0700

    2

--- app/src/main/java/com/example/a38_nguyenbavan_lap4/MainActivity.java
+++ app/src/main/java/com/example/a38_nguyenbavan_lap4/MainActivity.java
@@ -2,38 +2,64 @@ package com.example.a38_nguyenbavan_lap4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-EditText edtpassword;
+EditText edtpassword,edtuser;
 Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtpassword = findViewById(R.id.edtpassword);
+        edtuser = findViewById(R.id.edtuser);
         btnLogin = findViewById(R.id.btnLogin);
-        final String strpassword = edtpassword.getText().toString();
-        int dem=0;
-        for (int i=0; i<strpassword.length();i++){
-
-            if ((strpassword.charAt(i) <= 9) && (strpassword.charAt(i) >= 0)){
-                dem++;
-            }
 
 
-        }
-        final int finalDem = dem;
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               if(TextUtils.isEmpty(strpassword)) {
+               int dem=0;
+               int dem2=0;
+               int dem3=0;
+               int dem4=0;
+                String strpassword = edtpassword.getText().toString();
+                String struser = edtuser.getText().toString();
+               if(struser.length()==0) {
+                   edtpassword.setError("Your User");
+
+               }
+               for (int i=0; i<strpassword.length();i++){
+
+
+                   if ((strpassword.codePointAt(i)>=48)&&(strpassword.codePointAt(i)<=57)){
+                       dem++;
+                   }
+                   if ((strpassword.codePointAt(i)>=65)&&(strpassword.codePointAt(i)<=90)){
+                       dem2++;
+                   }
+                   if ((strpassword.codePointAt(i)>=97)&&(strpassword.codePointAt(i)<=122)){
+                       dem3++;
+                   }
+                   if ((strpassword.codePointAt(i)>0)&&(strpassword.codePointAt(i)<48)
+                           ||(strpassword.codePointAt(i)>57)&&(strpassword.codePointAt(i)<65)
+                           ||(strpassword.codePointAt(i)>90)&&(strpassword.codePointAt(i)<97)||
+                           (strpassword.codePointAt(i)>122)&&(strpassword.codePointAt(i)<255)){
+                       dem4++;
+                   }
+               }
+
+
+               if(strpassword.length()==0) {
                    edtpassword.setError("Your Password");
-                   return;
+
                }
                else
                    if(strpassword.length()<6)
@@ -41,10 +67,25 @@ Button btnLogin;
                        edtpassword.setError("Your Password Must Have 6 Characters");
                    }
                    else
-                       if( finalDem ==0){
+                       if(  dem ==0){
                            edtpassword.setError("Your Password Must Have 1 Number");
                        }
-                       
+                       else if(dem2 ==0){
+                           edtpassword.setError("Your Password Must Have 1 uppercase characters");
+                       }
+                       else if(dem3 ==0){
+                           edtpassword.setError("Your Password Must Have 1 lowercase characters");
+                       }
+                       else if(dem4 ==0){
+                           edtpassword.setError("Your Password Must Have 1 Special characters");
+                       }
+                       else{
+                           Toast.makeText(MainActivity.this, "Username: admin      Password: Admin123*", Toast.LENGTH_SHORT).show();
+                       }
+                       if(struser.equals("admin")==true&&strpassword.equals("Admin123*")==true){
+                           Intent intent = new Intent(getBaseContext(),CreateNewNote.class);
+                           startActivity(intent);
+                       }
            }
        });
 
